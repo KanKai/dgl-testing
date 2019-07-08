b1836750962899ad73918da7a1ab447126bd39d8
Jeerawat Keawsawat <gukaihero@gmail.com>
2019-07-08 10:04:37 +0700

    Change repo url

--- cypress/integration/features-file/dgl-login/login/login.js
+++ cypress/integration/features-file/dgl-login/login/login.js
@@ -1,5 +1,5 @@
 import { Given, When, And, Then } from 'cypress-cucumber-preprocessor/steps'
-import DGLLoginPage from '../../../pages/dgl-login-page'
+import DGLLoginPage from '../../../../pages/dgl-login-page'
 
 Given(/^I visit DGL Site$/, () => {
     DGLLoginPage.visit()
