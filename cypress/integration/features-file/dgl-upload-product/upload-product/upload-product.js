456faf38d18e16e6c54d97ed85cc752709f4ca82
Jeerawat Keawsawat <gukaihero@gmail.com>
2019-07-09 23:35:25 +0700

    update test feature product

--- cypress/integration/features-file/dgl-upload-product/upload-product/upload-product.js
+++ cypress/integration/features-file/dgl-upload-product/upload-product/upload-product.js
@@ -1,19 +1,56 @@
+/// <reference types="Cypress" />
 import { Given, When, And, Then } from 'cypress-cucumber-preprocessor/steps'
 import DGLUploadProductPage from '../../../../pages/dgl-upload-product-page'
 import DGLLoginPage from '../../../../pages/dgl-login-page'
 
-Given(/^I visit DGL Site$/, () => {
+Given(/^I visit DGL Site go to product page$/, () => {
+    cy.viewport('macbook-13')
+    cy.clearLocalStorage()
     DGLLoginPage.visit()
 })
 
 When(/^I perform login$/, dataSource => {
     dataSource.hashes().forEach(row => {
-        DGLLoginPage.performLogin(row.email, row.password)
+        cy.login(row.email, row.password, false)
     });
-    DGLLoginPage.clickRemember()
-    DGLLoginPage.clickLoginButton()
 })
 
 Then(/^Check DGL dashboard page$/, () => {
     cy.url().should('include', '/dashboard')
+})
+
+When(/^I jump to product page$/, () => {
+    DGLUploadProductPage.gotoProductPage()
+})
+
+When(/^Check DGL product page$/, () => {
+    DGLUploadProductPage.confirmProductPage()
+})
+
+When(/^I input product detail$/, () => {
+    DGLUploadProductPage.gotoCreateProductPage()
+    DGLUploadProductPage.importImage()
+    cy.fixture('dgl-product').as('_product')
+    cy.get('@_product').then(product => {
+        DGLUploadProductPage.performProduct(product.create)
+    })
+})
+
+And(/^Press submit$/, () => {
+    DGLUploadProductPage.clickButtonSave()
+})
+
+When(/^Alert create successed$/, () => {
+})
+
+But(/^Alert create fail$/, () => {
+})
+
+When(/^I jump to product detail page$/, () => {
+})
+
+When(/^Product found$/, () => {
+})
+
+Then(/^Delete product$/, () => {
 })
--- cypress/pages/dgl-upload-product-page.js
+++ cypress/pages/dgl-upload-product-page.js
@@ -2,10 +2,68 @@
 import faker from 'faker'
 
 class DGLUploadProduct {
-    static mockDetail() {
-        return {
-            result: 'test'
-        }
+    static gotoProductPage() {
+        cy.get('.nb-menu').click()
+        cy.xpath('//*[@id="4"]/label').click()
+    }
+
+    static gotoCreateProductPage() {
+        cy.get('#btn-oe-save').find('span > mat-icon')
+            .should($x => {
+                expect($x).to.have.length(1)
+            })
+            .then($x => {
+                expect($x).to.have.text('add')
+            }).as('btnCreate')
+        cy.get('@btnCreate').click()  
+    }
+
+    static importImage() {
+        cy.fixture('asus.jpg','base64').then(fileContent => {
+            cy.get('#pd-input-file-image').upload(
+                {
+                    fileContent,
+                    fileName: 'asus.jpg',
+                    mimeType: 'image/jpg'
+                },
+                {
+                    uploadType:'input'
+                }
+            )
+        })
+    }
+
+    static performProduct(productInfo) {
+        cy.get('#pd-input-productgroup-name').debug().type(productInfo.productgroup, { delay: 100 });
+        cy.get('#pd-input-productunit-name').debug().type(productInfo.productunit, { delay: 100 });
+        cy.get('#pd-input-nameTH').debug().type(productInfo.nameTH, { delay: 100 });
+        cy.get('#pd-input-nameEN').debug().type(productInfo.nameEN, { delay: 100 });
+        cy.get('#pd-input-unitprice').debug().type(productInfo.unitprice, { delay: 100 });
+        cy.get('#pd-textarea-remark').debug().type(productInfo.remark, { delay: 100 });
+    }
+
+    static clickButtonSave() {
+        cy.get('#pd-button-submit').scrollIntoView()
+    }
+
+    static checkCompany() {
+        cy.server()
+        cy.route({
+            method: 'GET',
+            url: '/api/v1/company'
+        }).as('company')
+        cy.get('@company').then(company => {
+            cy.log(company)
+        })
+    }
+
+    static confirmProductPage() {
+        cy.xpath("/html/body/app-root/app-home-layout/app-layout/nb-layout/div/div/div/div/div/nb-layout-column/div/app-product/div[1]").find('label')
+            .should($x => {
+                expect($x).to.have.length(1)
+            }).then($x => {
+                expect($x).to.have.text('สินค้า')
+            })
     }
 }
 
